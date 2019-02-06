d94306bd52efd55867864d219ca8ef24194a58c7
Piotr Nastaj <piotr@nastaj.net>
2019-02-06 17:01:30 +0100

    Connect to mLab

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase');
+mongoose.connect('mongodb://Piet:123abc@ds125125.mlab.com:25125/database-1');
 
 //new user Schema
 const userSchema = new Schema({
